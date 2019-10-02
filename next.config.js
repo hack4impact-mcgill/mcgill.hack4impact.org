7fbe870b57cf45953a56f0095f0545f3385e4603
kraglalbert <albert.kragl@mail.mcgill.ca>
2019-10-02 17:20:11 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@ module.exports = {
   webpack: config => {
     // Fixes npm packages that depend on `fs` module
     config.node = {
-      fs: "empty"
+      fs: "empty",
+      assetPrefix: isProd
+        ? "https://hack4impact-mcgill.github.io/mcgill.hack4impact.org"
+        : ""
     };
 
     return config;
