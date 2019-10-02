9cba31f7bf4fad1ebc03b2563f09eea3325fd34f
kraglalbert <albert.kragl@mail.mcgill.ca>
2019-10-02 18:11:16 -0400

    Remove assetPrefix

--- next.config.js
+++ next.config.js
@@ -2,10 +2,7 @@ module.exports = {
   webpack: config => {
     // Fixes npm packages that depend on `fs` module
     config.node = {
-      fs: "empty",
-      assetPrefix: isProd
-        ? "https://hack4impact-mcgill.github.io/mcgill.hack4impact.org"
-        : ""
+      fs: "empty"
     };
 
     return config;
